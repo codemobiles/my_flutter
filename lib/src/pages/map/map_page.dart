1f53b02d999684e56c7b3b68a814feec5bf58198
iBlurBlur <tanakorn.ngam@gmail.com>
2020-09-17 14:32:01 +0700

    prepare map

--- lib/src/pages/map/map_page.dart
+++ lib/src/pages/map/map_page.dart
@@ -12,36 +12,53 @@ class MapPageState extends State<MapPage> {
   Completer<GoogleMapController> _controller = Completer();
 
   static final CameraPosition _kGooglePlex = CameraPosition(
-    target: LatLng(37.42796133580664, -122.085749655962),
-    zoom: 14.4746,
+    target: LatLng(13.7625754, 100.4938512),
+    zoom: 12.4746,
   );
 
-  static final CameraPosition _kLake = CameraPosition(
-      bearing: 192.8334901395799,
-      target: LatLng(37.43296265331129, -122.08832357078792),
-      tilt: 59.440717697143555,
-      zoom: 19.151926040649414);
+  Set<Marker> _markers;
+
+  @override
+  void initState() {
+    _markers = Set();
+    feedNetwork();
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
       body: GoogleMap(
+        markers: _markers,
         mapType: MapType.hybrid,
         initialCameraPosition: _kGooglePlex,
         onMapCreated: (GoogleMapController controller) {
           _controller.complete(controller);
         },
       ),
-      floatingActionButton: FloatingActionButton.extended(
-        onPressed: _goToTheLake,
-        label: Text('To the lake!'),
-        icon: Icon(Icons.directions_boat),
-      ),
     );
   }
 
-  Future<void> _goToTheLake() async {
-    final GoogleMapController controller = await _controller.future;
-    controller.animateCamera(CameraUpdate.newCameraPosition(_kLake));
+  void feedNetwork() async {
+    await Future.delayed(Duration(seconds: 5));
+    final List<LatLng> dummyLocations = [];
+    dummyLocations.add(LatLng(13.7625754, 100.4938512));
+    dummyLocations.add(LatLng(13.728139, 100.5308373));
+
+    dummyLocations.forEach((latLng) {
+      Marker marker = Marker(
+        markerId: MarkerId(UniqueKey().toString()),
+        position: latLng,
+        infoWindow: InfoWindow(
+          title: "CM",
+          snippet: "09:00 - 18:00",
+        )
+      );
+      _markers.add(marker);
+    });
+
+    setState(() {
+
+    });
   }
 }
