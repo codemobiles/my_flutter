9ce085d4735c3761cb18d427f1dd11eaf636505e
iBlurBlur <tanakorn.ngam@gmail.com>
2020-09-17 16:02:23 +0700

    tracking location and custom marker

--- lib/src/pages/map/map_page.dart
+++ lib/src/pages/map/map_page.dart
@@ -1,9 +1,12 @@
 import 'dart:async';
+import 'dart:typed_data';
+import 'dart:ui';
 
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:location/location.dart';
+import 'package:my_flutter/src/commons/constants.dart';
 
 class MapPage extends StatefulWidget {
   @override
@@ -20,6 +23,10 @@ class MapPageState extends State<MapPage> {
 
   Set<Marker> _markers;
 
+  StreamSubscription<LocationData> _locationSubscription;
+
+  double _height;
+
   @override
   void initState() {
     _markers = Set();
@@ -28,7 +35,14 @@ class MapPageState extends State<MapPage> {
   }
 
   @override
+  void dispose() {
+    _locationSubscription?.cancel();
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
+    _height = MediaQuery.of(context).size.height;
     return new Scaffold(
       body: Stack(
         children: [
@@ -46,7 +60,8 @@ class MapPageState extends State<MapPage> {
           ),
           FloatingActionButton(
             onPressed: () {
-              oneTimeLocation();
+              //oneTimeLocation();
+              trackingLocation();
             },
             child: Icon(Icons.my_location),
           )
@@ -93,5 +108,117 @@ class MapPageState extends State<MapPage> {
   Future<void> animateCamera({LatLng position}) async {
     GoogleMapController google = await _controller.future;
     google.animateCamera(CameraUpdate.newLatLngZoom(position, 12));
+  }
+
+  trackingLocation() async {
+    if (_locationSubscription != null) {
+      setState(() {
+        _locationSubscription.cancel();
+        _locationSubscription = null;
+        _markers?.clear();
+      });
+    } else {
+      final Location _locationService = Location();
+      await _locationService.changeSettings(
+        accuracy: LocationAccuracy.high,
+        interval: 800,
+        distanceFilter: 100,
+      ); // meters.
+
+      try {
+        if (await _locationService.serviceEnabled()) {
+          if (await _locationService.requestPermission() ==
+              PermissionStatus.granted) {
+            _locationSubscription = _locationService.onLocationChanged.listen(
+              (LocationData result) async {
+                _markers?.clear();
+                final latLng = LatLng(result.latitude, result.longitude);
+                await addMarker(
+                  latLng,
+                );
+                setState(() {
+                  animateCamera(position: latLng);
+                });
+
+                print(latLng);
+              },
+            );
+          } else {
+            print('Permission denied');
+          }
+        } else {
+          bool serviceStatusResult = await _locationService.requestService();
+          print("Service status activated after request: $serviceStatusResult");
+          if (serviceStatusResult) {
+            trackingLocation();
+          } else {
+            print('Service denied');
+          }
+        }
+      } on PlatformException catch (e) {
+        print('trackingLocation error: ${e.message}');
+
+        if (e.code == 'PERMISSION_DENIED') {
+          return print('Permission denied');
+        }
+
+        if (e.code == 'SERVICE_STATUS_ERROR') {
+          return print('Service error');
+        }
+      }
+    }
+  }
+
+  addMarker(
+    LatLng position, {
+    String title = 'none',
+    String snippet = 'none',
+    String pinAsset = "assets/images/header_1.png",
+    bool isShowInfo = false,
+  }) async {
+    final Uint8List markerIcon = await getBytesFromAsset(
+      pinAsset,
+      width: _height * 50 ~/ 100,
+    );
+    final BitmapDescriptor bitmap = BitmapDescriptor.fromBytes(markerIcon);
+
+    _markers.add(
+      Marker(
+        markerId: MarkerId(position.toString()),
+        // important. unique id
+        position: position,
+        infoWindow: isShowInfo
+            ? InfoWindow(
+                title: title,
+                snippet: snippet,
+                onTap: () {
+                  // _launchMaps(
+                  //   lat: position.latitude,
+                  //   lng: position.longitude,
+                  // );
+                },
+              )
+            : null,
+        icon: bitmap,
+        onTap: () {
+          print('lat: ${position.latitude}, lng: ${position.longitude}');
+        },
+      ),
+    );
+  }
+
+  Future<Uint8List> getBytesFromAsset(
+    String path, {
+    int width,
+  }) async {
+    ByteData data = await rootBundle.load(path);
+    Codec codec = await instantiateImageCodec(
+      data.buffer.asUint8List(),
+      targetWidth: width,
+    );
+    FrameInfo fi = await codec.getNextFrame();
+    return (await fi.image.toByteData(format: ImageByteFormat.png))
+        .buffer
+        .asUint8List();
   }
 }
